8eb84e91dec653aecbfa5220fee10e5fa0d6806d
Ken Howard <ken@simplicatedweb.com>
2015-07-17 22:41:13 -0700

    load the Express "Hello World!" example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 /**
  * This file bootstraps the application
  */
+ 
+import express = require('express');
+ 
+var app = express();
+
+app.get('/', (request: express.Request, response: express.Response) => {
+	response.send('Hello World!');
+});
+
+var server = app.listen(3000, () => {
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('App listening on port %s', port);
+});
