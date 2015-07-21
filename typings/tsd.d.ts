e5fcbb414e0a797558c0a203395adf9328f34181
Ken Howard <ken@simplicatedweb.com>
2015-07-20 22:21:33 -0700

    Updated TypeScript configuration to use TS 1.5

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,8 +1,8 @@
 
 /// <reference path="app.d.ts" />
-/// <reference path="node/node.d.ts" />
 /// <reference path="express/express.d.ts" />
 /// <reference path="mongoose/mongoose.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="nconf/nconf.d.ts" />
+/// <reference path="node/node.d.ts" />
